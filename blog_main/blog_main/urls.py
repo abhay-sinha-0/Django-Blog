771b5342c9aa720ef5e9d6a94ed97bbb410082d5
Abhay Kumar <abhay.sinha03@gmail.com>
2026-02-20 12:26:57 +0530

    blogs and search features

--- blog_main/blog_main/urls.py
+++ blog_main/blog_main/urls.py
@@ -21,12 +21,17 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
+from blogs import views as BlogsViews
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('category/', include('blogs.urls')),
+    # path('category/', include('blogs.urls')),
+    path('', include('blogs.urls')),
+    path('<slug:slug>/', BlogsViews.blogs, name='blogs'),
+    # search endpoint
+    path('blogs/search/', BlogsViews.search, name='search'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve static files in development
--- blog_main/blog_main/views.py
+++ blog_main/blog_main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from assignments.models import About
 from blogs.models import Category,Blog
 
 def home(request): 
@@ -9,9 +10,17 @@ def home(request):
     # print(categories)
     posts = Blog.objects.filter(is_featured=False, status='Published')
     # print(posts)
+    
+    # Fetch about us data
+    try:
+        about = About.objects.get()
+    except:
+        about=None
+    
     context = {
         # 'categories': categories, 
         'featured_post': featured_post,
         'posts': posts,   
+        'about': about,
     }
     return render(request, 'home.html', context)
