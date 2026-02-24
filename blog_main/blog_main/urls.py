bd7f6b3894f0b917ae7b9bbd4198e92c6ab7400e
Abhay Kumar <abhay.sinha03@gmail.com>
2026-02-24 12:23:08 +0530

    Categories crud Functionality

--- blog_main/blog_main/urls.py
+++ blog_main/blog_main/urls.py
@@ -35,6 +35,10 @@ urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    
+    # Dashboards
+    path('dashboard/', include('dashboards.urls')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve static files in development
